a1e3a9b50ec2548a7bf99559889634291b01bdf5
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-01-19 16:48:42 +0000

    feat: Shipment API constants

--- js/shipment.js
+++ js/shipment.js
@@ -1,3 +1,28 @@
+/**
+ * Represents the metric system.
+ */
+export const METRIC = "metric";
+
+/**
+ * Represents the imperial system.
+ */
+export const IMPERIAL = "imperial";
+
+/**
+ * Represents the Delivered-At-Place (DAP) incoterm code.
+ */
+export const INCOTERM_DAP = "DAP";
+
+/**
+ * The code representing a domestic express shipment.
+ */
+export const DOMESTIC_EXPRESS = "N";
+
+/**
+ * The code representing a domestic express shipment.
+ */
+export const WORLRDWIDE_EXPRESS = "D";
+
 export const ShipmentAPI = superclass =>
     class extends superclass {
         async createShipment(payload, options = {}) {
